555710d268e3fc255a1e8a14c2d6cc4d2465ab5a
Louise McDonald <louisemcdonald1@gmail.com>
2016-04-18 15:23:19 +0100

    Demo 3b - separating out JS

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//Declare variable for listening to button1
+var event1 = document.getElementById("button1");
+//Set up event listener on button1 click
+event1.addEventListener('click',myfirst,false);
+
+function myfirst(){
+    //check event listener is working
+    //alert("event listener is working");
+    //Get the value from the text box and save it to local variable
+    var userinput1 = document.getElementById("input1").value;
+    //Display the text box value on the screen
+    document.getElementById("demo").innerHTML = userinput1;
+}
